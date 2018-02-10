280057b41a2ad207f828f56c8b11b829fd68c012
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-02-10 14:46:26 -0800

    Adding auxiliary functions

--- sandbox/midterm_practice/mt01.c
+++ sandbox/midterm_practice/mt01.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "functions.h"
 
 unsigned int sum(int);
 short allDifferent(unsigned int[], unsigned int);
@@ -9,8 +10,21 @@ int main() {
     printf("sum(654321) == 21 ? %s\n",
             sum(654321) == 21 ? "True" : "False");
 
-    printf("sum(-654321) = %d\n", 
+    printf("sum(-654321) = %d\n\n", 
             sum(-654321));
+
+
+    unsigned int a[5] = { 17, 22, 33, 41, 9 };
+    printUIntArray(a, sizeof(a));
+    puts("");
+
+    short trueOrFalse = allDifferent(a, 5);
+    puts("");
+
+    printf("allDifferent(a, 5) == 1 ? %s\n",
+            trueOrFalse == 1 ? "True" : "False");
+    printf("allDifferent(a, 5) =  %d\n", 
+            trueOrFalse);
 
 
     return 0;
@@ -24,18 +38,29 @@ unsigned int sum(int n)
     unsigned int sumOfDigits = 0;
     do {
         sumOfDigits += n % 10;
-        printf("sumOfDigits = %d\n", sumOfDigits);
     } while ((n /= 10) != 0);
-
+    
+    printf("sumOfDigits = %d\n", sumOfDigits);
     return abs(sumOfDigits);
 }
 
-short allDifferent(unsigned int a[], unsigned int na) {
+short allDifferent(unsigned int *a, unsigned int na) {
 
     unsigned int sumsOfDigits[na];
+    initializeUIntArray(sumsOfDigits, na);
     // number of comparisons would increase per each iteration
-    for (int i = 0; i < na; i++) {
+    int temp;
 
+    for (int i = 0; i < na; i++) {
+        printf("Current number: %d, ", a[i]);
+        temp = sum(a[i]);
+        if (i > 0)
+            for (int j = 0; j < i; j++)             // sequential search for now - assuming non sorted array
+                if (sumsOfDigits[j] == temp)
+                    return 0;
+
+        sumsOfDigits[i] = temp;             // should do mergesort insert && binary search
+        printUIntArray(sumsOfDigits, sizeof(sumsOfDigits));
     }
 
     return 1;
