075711d94e25ad8d9d19696a672e129b434e9043
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-23 16:00:20 -0800

    Created a prototype of the maxlen function

--- homeworks/HW2/hw2-problem1/hw2-problem1.c
+++ homeworks/HW2/hw2-problem1/hw2-problem1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int maxlen(int*, int);
+int maxlen(int*, unsigned int);
 
 int main()
 {
@@ -8,8 +8,20 @@ int main()
     return 0;
 }
 
-int maxlen(int* a, int)
+int maxlen(int* a, unsigned int n)
 {
+    unsigned int max_count, current_count, i;
+    max_count = 0;
+    current_count = 1;
 
-    return 0;
+    for (i = 1; i < n; i++) 
+    {
+        if (a[i] == a[i-1])
+            current_count++;
+        else if (current_count > max_count)
+            max_count = current_count;
+        else
+            current_count = 1;
+    }
+    return max_count;
 }
