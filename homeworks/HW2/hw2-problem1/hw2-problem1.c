4db4d0c62cbf6d2105aa50ae0fe6b4444d3db306
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-25 12:06:32 -0800

    Minor formatting changes.

--- homeworks/HW2/hw2-problem1/hw2-problem1.c
+++ homeworks/HW2/hw2-problem1/hw2-problem1.c
@@ -8,22 +8,26 @@ int main()
     int a[13] = { 1, 1, 1, 2, 3, 3, 5, 6, 6, 6, 6, 7, 9 };
     printf("Array a: ");
     printIntArray(a, sizeof(a));
-    printf("Max sequence length of array a = %d\n\n", maxlen(a, 13));
+    printf("Max sequence length of array a = %d\n\n",
+           maxlen(a, 13));
 
-    int b[0] = {};                   // testing the edge case with an empty array
+    int b[0] = {};                              // testing the edge case with an empty array
     printf("Array b: ");
     printIntArray(b, sizeof(b));
-    printf("Max sequence length of array b = %d\n\n", maxlen(b, 0));
+    printf("Max sequence length of array b = %d\n\n",
+           maxlen(b, 0));
 
-    int c[1] = { 12 };              // testing the edge case with just 1 element in the array
+    int c[1] = { 12 };                          // testing the edge case with just 1 element in the array
     printf("Array c: ");
     printIntArray(c, sizeof(c));
-    printf("Max sequence length of array c = %d\n\n", maxlen(c, 1));
+    printf("Max sequence length of array c = %d\n\n",
+           maxlen(c, 1));
 
     int d[12] = { -49, -15, 16, 16, 16, 17, 17, 18, 18, 18, 19, 20 };
     printf("Array d: ");
     printIntArray(d, sizeof(d));
-    printf("Max sequence length of array d = %d\n\n", maxlen(d, 12));
+    printf("Max sequence length of array d = %d\n\n",
+           maxlen(d, 12));
 
     return 0;
 }
@@ -37,7 +41,7 @@ unsigned int maxlen(int *a, unsigned int n)
     i = max_count = 0;
     current_count = 1;
 
-    printf("\ta[%d]=%d; \tcurrent_count=%d; \tmax_count=%d\n", i, a[i], current_count, max_count);          // printing element 1
+    printf("\ta[%d]=%d; \tcurrent_count=%d; \tmax_count=%d\n", i, a[i], current_count, max_count);
 
     for (i = 1; i < n; ++i)
     {
@@ -52,7 +56,7 @@ unsigned int maxlen(int *a, unsigned int n)
             current_count = 1;                  // reseting the counter
         }
 
-        printf("\ta[%d]=%d; \tcurrent_count=%d; \tmax_count=%d\n", i, a[i], current_count, max_count);          // printing elements 2..n
+        printf("\ta[%d]=%d; \tcurrent_count=%d; \tmax_count=%d\n", i, a[i], current_count, max_count);
     }
     return max_count;
 }
--- homeworks/hw2/hw2-problem1/functions.c
+++ homeworks/hw2/hw2-problem1/functions.c
@@ -9,11 +9,6 @@ void printIntArray(int *array, unsigned int size)
     genericPrintNumArray(array, size, sizeof(int));
 }
 
-void printUIntArray(unsigned int *array, unsigned int size) 
-{
-    genericPrintNumArray(array, size, sizeof(int));
-}
-
 void genericPrintNumArray(void *object, unsigned int size, unsigned int elem_size) 
 {
     char *p = (char *) object;
@@ -21,20 +16,5 @@ void genericPrintNumArray(void *object, unsigned int size, unsigned int elem_siz
         printf("%d\t", *p);
         p += elem_size;
     }
-
-    //the same as:
-//    printf("\n");
-//    for (char *p = (char *) object; p < (char *) object + size; p += elem_size) {
-//        printf("%d\t", *p);
-//    }
-
-
-
     printf("\n");
-}
-
-void initializeUIntArray(unsigned int *array, unsigned int size) 
-{
-    for (int i = 0; i < size; i++)
-        array[i] = 0;
 }
