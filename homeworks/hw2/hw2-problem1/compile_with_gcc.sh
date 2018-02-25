214c7dae3a1fd1d39f68ddceb893a232aa519de6
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-25 10:10:36 -0800

    Auxiliary files

---
+++ homeworks/hw2/hw2-problem1/compile_with_gcc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -Wall -std=c99 $1 functions.c -O3
---
+++ homeworks/hw2/hw2-problem1/functions.h
@@ -0,0 +1,4 @@
+void printIntArray(int *, unsigned int);
+void printUIntArray(unsigned int *, unsigned int);
+void genericPrintNumArray(void *, unsigned int, unsigned int);
+void initializeUIntArray(unsigned int *, unsigned int);
