e13331c18e8e93c4804b23e4f5d3e1e21a57671a
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-28 11:44:21 -0800

    Beta 2: imporved version, with the reversed bit pattern.

--- homeworks/HW2/hw2-problem2/hw2-problem2.c
+++ homeworks/HW2/hw2-problem2/hw2-problem2.c
@@ -15,48 +15,85 @@ unsigned int getNumberOfDigits(unsigned int);
  * than the storage in brute-force approach where each digit is represented by
  * a 2D array of 8x8 characters, with 64 bytes of storage per digit.
  */
-const unsigned char BIG_DIGITS[NUMBER_OF_ROWS][10] = {
+// const unsigned char BIG_DIGITS[NUMBER_OF_ROWS][10] = {
+//        {       // row 0 of all 10 digits
+//                0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u,
+//                0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u
+//        },
+//
+//        {       // row 1 of all 10 digits
+//                0b00111110u, 0b00001100u, 0b00011110u, 0b00011110u, 0b00000110u,
+//                0b00111111u, 0b00011110u, 0b01111111u, 0b00011110u, 0b00011110u
+//        },
+//
+//        {       // row 2 of all 10 digits
+//                0b01100011u, 0b00011100u, 0b00110011u, 0b00110011u, 0b00001110u,
+//                0b00110000u, 0b00110011u, 0b00000011u, 0b00110011u, 0b00110011u
+//        },
+//
+//        {       // row 3 of all 10 digits
+//                0b01100011u, 0b00001100u, 0b00110011u, 0b00000011u, 0b00010110u,
+//                0b00110000u, 0b00110000u, 0b00000110u, 0b00110011u, 0b00110011u
+//        },
+//
+//        {       // row 4 of all 10 digits
+//                0b01100011u, 0b00001100u, 0b00000110u, 0b00001100u, 0b00110110u,
+//                0b00111110u, 0b00111110u, 0b00001100u, 0b00011110u, 0b00011111u
+//        },
+//
+//        {       // row 5 of all 10 digits
+//                0b01100011u, 0b00001100u, 0b00001100u, 0b00000011u, 0b01100110u,
+//                0b00000011u, 0b00110011u, 0b00011000u, 0b00110011u, 0b00000011u
+//        },
+//
+//        {       // row 6 of all 10 digits
+//                0b01100011u, 0b00001100u, 0b00011000u, 0b00110011u, 0b01111111u,
+//                0b00000011u, 0b00110011u, 0b00110000u, 0b00110011u, 0b00110011u
+//        },
+//
+//        {       // row 7 of all 10 digits
+//                0b00111110u, 0b00111111u, 0b00111111u, 0b00011110u, 0b00000110u,
+//                0b00111110u, 0b00011110u, 0b01100000u, 0b00011110u, 0b00011110u
+//        }
+//};
 
-        {       // row 0 of all 10 digits
+// reverse digits:
+const unsigned char BIG_DIGITS[8][10] = {
+
+        {       // row0 of all 10 digits
                 0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u,
                 0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u, 0b00000000u
         },
-
-        {       // row 1 of all 10 digits
-                0b00111110u, 0b00001100u, 0b00011110u, 0b00011110u, 0b00000110u,
-                0b00111111u, 0b00011110u, 0b01111111u, 0b00011110u, 0b00011110u
+        {       // row1 of all 10 digits
+                0b01111100u, 0b00110000u, 0b01111000u, 0b01111000u, 0b01100000u,
+                0b11111100u, 0b01111000u, 0b11111110u, 0b01111000u, 0b01111000u
         },
-
-        {       // row 2 of all 10 digits
-                0b01100011u, 0b00011100u, 0b00110011u, 0b00110011u, 0b00001110u,
-                0b00110000u, 0b00110011u, 0b00000011u, 0b00110011u, 0b00110011u
+        {       // row2 of all 10 digits
+                0b11000110u, 0b00111000u, 0b11001100u, 0b11001100u, 0b01110000u,
+                0b00001100u, 0b11001100u, 0b11000000u, 0b11001100u, 0b11001100u
         },
-
-        {       // row 3 of all 10 digits
-                0b01100011u, 0b00001100u, 0b00110011u, 0b00000011u, 0b00010110u,
-                0b00110000u, 0b00110000u, 0b00000110u, 0b00110011u, 0b00110011u
+        {       // row3 of all 10 digits
+                0b11000110u, 0b00110000u, 0b11001100u, 0b11000000u, 0b01101000u,
+                0b00001100u, 0b00001100u, 0b01100000u, 0b11001100u, 0b11001100u
         },
-
-        {       // row 4 of all 10 digits
-                0b01100011u, 0b00001100u, 0b00000110u, 0b00001100u, 0b00110110u,
-                0b00111110u, 0b00111110u, 0b00001100u, 0b00011110u, 0b00011111u
+        {       // row4 of all 10 digits
+                0b11000110u, 0b00110000u, 0b01100000u, 0b00110000u, 0b01101100u,
+                0b01111100u, 0b01111100u, 0b00110000u, 0b01111000u, 0b11111000u
         },
-
-        {       // row 5 of all 10 digits
-                0b01100011u, 0b00001100u, 0b00001100u, 0b00000011u, 0b01100110u,
-                0b00000011u, 0b00110011u, 0b00011000u, 0b00110011u, 0b00000011u
+        {       // row5 of all 10 digits
+                0b11000110u, 0b00110000u, 0b00110000u, 0b11000000u, 0b01100110u,
+                0b11000000u, 0b11001100u, 0b00011000u, 0b11001100u, 0b11000000u
         },
-
-        {       // row 6 of all 10 digits
-                0b01100011u, 0b00001100u, 0b00011000u, 0b00110011u, 0b01111111u,
-                0b00000011u, 0b00110011u, 0b00110000u, 0b00110011u, 0b00110011u
+        {       // row6 of all 10 digits
+                0b11000110u, 0b00110000u, 0b00011000u, 0b11001100u, 0b11111110u,
+                0b11000000u, 0b11001100u, 0b00001100u, 0b11001100u, 0b11001100u
         },
-
-        {       // row 7 of all 10 digits
-                0b00111110u, 0b00111111u, 0b00111111u, 0b00011110u, 0b00000110u,
-                0b00111110u, 0b00011110u, 0b01100000u, 0b00011110u, 0b00011110u
+        {       // row7 of all 10 digits
+                0b01111100u, 0b11111100u, 0b11111100u, 0b01111000u, 0b01100000u,
+                0b01111100u, 0b01111000u, 0b00000110u, 0b01111000u, 0b01111000u
         }
 };
+
 
 int main()
 {
@@ -73,7 +110,8 @@ void BigInt(unsigned int n)
 {
     unsigned int numOfDigits, c;
     c = numOfDigits = getNumberOfDigits(n);
-    int decimals[numOfDigits], bitPattern[NUMBER_OF_BITS];
+//    int decimals[numOfDigits], bitPattern[NUMBER_OF_BITS];
+    int decimals[numOfDigits];
 
     // decomposing the number into an array of decimal digits
     do {
@@ -86,13 +124,18 @@ void BigInt(unsigned int n)
     {
         for (int digit = 0; digit < numOfDigits; digit++)
         {
-            // iteratively extracting each bit, starting from the least significant position
-            for (int bit = 0; bit < NUMBER_OF_BITS; bit++)
-                bitPattern[bit] = (BIG_DIGITS[row][decimals[digit]] >> bit) & 1;
+            // solution for non-reversed digits:
+//            // iteratively extracting each bit, starting from the least significant position
+//            for (int bit = 0; bit < NUMBER_OF_BITS; bit++)
+//                bitPattern[bit] = (BIG_DIGITS[row][decimals[digit]] >> bit) & 1;
+//
+//            // printing the most significant bits first
+//            for (int bit = NUMBER_OF_BITS; bit >= 0; bit--)
+//                printf("%c", bitPattern[bit] == 1 ? '@' : ' ');
 
-            // printing the most significant bits first
-            for (int bit = NUMBER_OF_BITS; bit >= 0; bit--)
-                printf("%c", bitPattern[bit] == 1 ? '@' : ' ');
+            // iteratively extracting each bit from the bit pattern and printing it
+            for (int bit = 0; bit < NUMBER_OF_BITS; bit++)
+                printf("%c", ((BIG_DIGITS[row][decimals[digit]] >> bit) & 1) == 1 ? '@' : ' ');
         }
         puts("");
     }
