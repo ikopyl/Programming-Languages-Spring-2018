038aed1f117385de60fb55217547b965f422a5b5
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-28 22:13:38 -0800

    HW2.3 is ready.

--- homeworks/HW2/hw2-problem3/hw2-problem3.c
+++ homeworks/HW2/hw2-problem3/hw2-problem3.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
+#include "functions.h"
 
 unsigned int reduce(int *, unsigned int);
-void findTop3MaxValues(int *, unsigned int, int *, int *, int *);
+void findTop3MaxValuesInArray(int *, unsigned int, int *, int *, int *);
+void nullifyTop3MaxValuesInArray(int *, unsigned int, int, int, int);
 unsigned int moveZeroesToEndOfArray(int *, unsigned int);
 
 int main()
 {
-
-    int max1, max2, max3;
-    max1 = max2 = max3 = 0;
-
     int a[16] = { 9, 1, 1, 6, 7, 1, 2, 3, 3, 5, 6, 6, 6, 6, 7, 9 };
-    findTop3MaxValues(a, 16, &max1, &max2, &max3);
+    printIntArray(a, sizeof(a));
 
-    printf("%d %d %d", max1, max2, max3);
+    int reducedN = reduce(a, 16);
+    printIntArray(a, reducedN * sizeof(int));
+    printf("reduced n = %d\n", reducedN);
 
     return 0;
 }
 
 unsigned int reduce(int * a, unsigned int n)
 {
+    int max1, max2, max3;
+    max1 = max2 = max3 = 0;
 
+    findTop3MaxValuesInArray(a, 16, &max1, &max2, &max3);
+    nullifyTop3MaxValuesInArray(a, 16, max1, max2, max3);
 
     return moveZeroesToEndOfArray(a, n);
 }
 
 /**
+ * Setting all occurrences of numbers max1, max2, max3 in the array to zero.
+ */
+void nullifyTop3MaxValuesInArray(int * a, unsigned int n, int max1, int max2, int max3)
+{
+    for (int i = 0; i < n; i++)
+        if (a[i] == max1 || a[i] == max2 || a[i] == max3)
+            a[i] = 0;
+}
+
+/**
  * Finding the first 3 maximum values of an array.
  */
-void findTop3MaxValues(int * a, unsigned int n, int *max1, int *max2, int *max3)
+void findTop3MaxValuesInArray(int * a, unsigned int n, int *max1, int *max2, int *max3)
 {
     *max1 = *max2 = *max3 = 0;
     for (int i = 0; i < n; i++)
