6bf70dcca1dd5cd9fb032e2d99867366cbcdc94e
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-28 21:55:16 -0800

    Function of finding the first 3 maximum values is ready.

--- homeworks/HW2/hw2-problem3/hw2-problem3.c
+++ homeworks/HW2/hw2-problem3/hw2-problem3.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 unsigned int reduce(int *, unsigned int);
+void findTop3MaxValues(int *, unsigned int, int *, int *, int *);
 unsigned int moveZeroesToEndOfArray(int *, unsigned int);
 
 int main()
 {
+
+    int max1, max2, max3;
+    max1 = max2 = max3 = 0;
+
+    int a[16] = { 9, 1, 1, 6, 7, 1, 2, 3, 3, 5, 6, 6, 6, 6, 7, 9 };
+    findTop3MaxValues(a, 16, &max1, &max2, &max3);
+
+    printf("%d %d %d", max1, max2, max3);
 
     return 0;
 }
@@ -12,7 +21,32 @@ int main()
 unsigned int reduce(int * a, unsigned int n)
 {
 
+
     return moveZeroesToEndOfArray(a, n);
+}
+
+/**
+ * Finding the first 3 maximum values of an array.
+ */
+void findTop3MaxValues(int * a, unsigned int n, int *max1, int *max2, int *max3)
+{
+    *max1 = *max2 = *max3 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > *max1)
+        {
+            *max3 = *max2;
+            *max2 = *max1;
+            *max1 = a[i];
+        }
+        else if (a[i] > *max2 && a[i] < *max1)
+        {
+            *max3 = *max2;
+            *max2 = a[i];
+        }
+        else if (a[i] > *max3 && a[i] < *max2)
+            *max3 = a[i];
+    }
 }
 
 /**
