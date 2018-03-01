1f10b393c1a77cfe7ffe4d5bffb66c2b4ac1495a
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-28 20:54:32 -0800

    Going greedy with moving the zeroes to the end of array.

--- homeworks/HW2/hw2-problem3/hw2-problem3.c
+++ homeworks/HW2/hw2-problem3/hw2-problem3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 unsigned int reduce(int *, unsigned int);
-void moveZeroesToEndOfArray(int *, unsigned int);
+unsigned int moveZeroesToEndOfArray(int *, unsigned int);
 
 int main()
 {
@@ -16,16 +16,15 @@ unsigned int reduce(int * a, unsigned int n)
     return reducedArraySize;
 }
 
-void moveZeroesToEndOfArray(int * a, unsigned int n)
+unsigned int moveZeroesToEndOfArray(int * a, unsigned int n)
 {
     unsigned int indexOfNullValue = 0;
 
-    // moving all non-zero values to the beginning of array
+    // copying all non-zero values to the beginning of array
     for (int i = 0; i < n; i++)
         if (a[i] != 0)
             a[indexOfNullValue++] = a[i];
 
-    // nullifying the remaining part of array
-    while (indexOfNullValue < n)
-        a[indexOfNullValue++] = 0;
+    // returning the size of reduced array
+    return indexOfNullValue;
 }
