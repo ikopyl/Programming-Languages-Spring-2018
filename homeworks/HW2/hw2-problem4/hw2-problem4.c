19a870f5fef91f2ade23832f10875cbabaec6862
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-03-01 12:27:11 -0800

    Floor of log2(9).

--- homeworks/HW2/hw2-problem4/hw2-problem4.c
+++ homeworks/HW2/hw2-problem4/hw2-problem4.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() 
 {
+
+    printf("%f\n", log2(8));
+    printf("%f\n", log2(9));
+    printf("%d\n", (unsigned int) log2(9));         // floor of log2(9)
 
 
     return 0;
