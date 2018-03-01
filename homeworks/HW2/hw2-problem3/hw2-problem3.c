50e4257047d9057e7c3b4ce27b62c1085ebc5566
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-28 21:08:30 -0800

    Some code refactoring.

--- homeworks/HW2/hw2-problem3/hw2-problem3.c
+++ homeworks/HW2/hw2-problem3/hw2-problem3.c
@@ -11,24 +11,31 @@ int main()
 
 unsigned int reduce(int * a, unsigned int n)
 {
-    unsigned int reducedArraySize = 0;
 
-    return reducedArraySize;
+    return moveZeroesToEndOfArray(a, n);
 }
+
 /**
- * Removes all zeroes from the array by copying all non-zero values
- * to the beginning of the array and returning the total amount of
- * non-zero values (i.e. the size of reduced array).
+ * Moves all zeroes to the end of array by copying all non-zero
+ * values to the beginning of the array and returning the total
+ * amount of non-zero values (i.e. the size of reduced array).
  */
-unsigned int removeAllZeroes(int * a, unsigned int n)
+unsigned int moveZeroesToEndOfArray(int * a, unsigned int n)
 {
-    unsigned int indexOfNullValue = 0;
+    unsigned int indexOfNullValue, sizeOfReducedArray;
+    indexOfNullValue = sizeOfReducedArray = 0;
 
     // copying all non-zero values to the beginning of array
     for (int i = 0; i < n; i++)
         if (a[i] != 0)
             a[indexOfNullValue++] = a[i];
 
+    sizeOfReducedArray = indexOfNullValue;
+
+    // nullifying the remaining part of the array
+    while (indexOfNullValue < n)
+        a[indexOfNullValue++] = 0;
+
     // returning the size of reduced array
-    return indexOfNullValue;
+    return sizeOfReducedArray;
 }
