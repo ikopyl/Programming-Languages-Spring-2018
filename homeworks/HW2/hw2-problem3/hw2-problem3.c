244ad7b967cfe3e50803ec23bc4ab6d1cac13dd1
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-02-28 20:49:14 -0800

    Steady progress with the problem #3 - moving all zeroes to the end of the array.

--- homeworks/HW2/hw2-problem3/hw2-problem3.c
+++ homeworks/HW2/hw2-problem3/hw2-problem3.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+unsigned int reduce(int *, unsigned int);
+void moveZeroesToEndOfArray(int *, unsigned int);
+
 int main()
 {
 
     return 0;
+}
+
+unsigned int reduce(int * a, unsigned int n)
+{
+    unsigned int reducedArraySize = 0;
+
+    return reducedArraySize;
+}
+
+void moveZeroesToEndOfArray(int * a, unsigned int n)
+{
+    unsigned int indexOfNullValue = 0;
+
+    // moving all non-zero values to the beginning of array
+    for (int i = 0; i < n; i++)
+        if (a[i] != 0)
+            a[indexOfNullValue++] = a[i];
+
+    // nullifying the remaining part of array
+    while (indexOfNullValue < n)
+        a[indexOfNullValue++] = 0;
 }
