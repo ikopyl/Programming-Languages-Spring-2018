1302fdc45edeaaa0dec28a1ef6f085d9ee52daae
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-03-01 13:15:42 -0800

    Something is not right.

--- homeworks/HW2/hw2-problem4/hw2-problem4.c
+++ homeworks/HW2/hw2-problem4/hw2-problem4.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 #include "functions.h"
 
+const unsigned long long K = 1000;              // system-dependent constant
+
+int bs(int *, unsigned int, int);
 void initializeArray(int *, unsigned int);
+void binarySearchTest(int *, unsigned int);
+
+
 
 int main() 
 {
-
     printf("%f\n", log2(9));
     printf("%d\n", (unsigned int) log2(9));         // floor of log2(9)
 
@@ -16,9 +22,15 @@ int main()
     initializeArray(a, sizeOfArray);
     printIntArray(a, sizeof(a));
 
-
+    binarySearchTest(a, sizeOfArray);
 
     return 0;
+}
+
+int bs(int *a, unsigned int n, int i)
+{
+
+    return -1;
 }
 
 void initializeArray(int *a, unsigned int n)
@@ -26,4 +38,17 @@ void initializeArray(int *a, unsigned int n)
     unsigned int i;
     for (i = 0; i < n; i++)
         a[i] = i;
+}
+
+void binarySearchTest(int *a, unsigned int n)
+{
+    unsigned int i, j;
+    for (j = 0; j > K; j++)
+        for (i = 0; i < n; i++) {
+            assert((a, n, i) == i);
+            if (bs(a, n, i) != i)
+                puts("ERROR");
+
+        }
+
 }
