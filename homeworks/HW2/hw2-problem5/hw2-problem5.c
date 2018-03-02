08e847a210e4eef1136c8bac6a8c137ac2e1e468
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-03-01 22:43:15 -0800

    Making a steady progress on problem #5.

--- homeworks/HW2/hw2-problem5/hw2-problem5.c
+++ homeworks/HW2/hw2-problem5/hw2-problem5.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
+int rFib(int);
+unsigned long long int iterativeFib(unsigned long long int);
+
 int main()
 {
 
     return 0;
+}
+
+/**
+ * Recursive implementation of function
+ * computing Fibonacci numbers.
+ */
+int rFib(int n)
+{
+    return n <= 1 ? n : rFib(n - 1) + rFib(n - 2);
+}
+
+unsigned long long int iterativeFib(unsigned long long int n)
+{
+    unsigned long long int first, second, temp, i;
+
+    if (n <= 1)
+        return n;
+    else
+        for (i = 2; i <= n; i++)
+        {
+            temp = first + second;
+            first = second;
+            second = temp;
+        }
+    return temp;        // the n-th value in Fibonacci sequence
 }
