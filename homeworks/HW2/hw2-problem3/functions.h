a598acde1a66ef72e8e9e6069152c19c55a5cc01
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-03-06 10:11:30 -0800

    Minor fixes

--- homeworks/HW2/hw2-problem3/functions.h
+++ homeworks/HW2/hw2-problem3/functions.h
@@ -1,4 +1,3 @@
 void printIntArray(int *, unsigned int);
 void printUIntArray(unsigned int *, unsigned int);
 void genericPrintNumArray(void *, unsigned int, unsigned int);
-void initializeUIntArray(unsigned int *, unsigned int);
