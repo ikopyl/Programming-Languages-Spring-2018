4244f129c5343010305d6a31e8dc01d329244f1e
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-03-14 00:13:03 -0700

    sublist.pl

--- homeworks/HW3/prolog_sandbox/sublist.pl
+++ homeworks/HW3/prolog_sandbox/sublist.pl
@@ -1,13 +1,23 @@
 % recursive definitions of sublist prerequisites: prefix & suffix: 
 
-prefix([], _).
-prefix([X|Xs], [X|Ys]) :-
-    prefix(Xs, Ys).
+append1([], Ys, Ys).
+append1([X|Xs], Ys, [X|Zs]) :-
+    append1(Xs, Ys, Zs).
+
+%prefix([], _).
+%prefix([X|Xs], [X|Ys]) :-
+%    prefix(Xs, Ys).
+
+prefix(Xs, Ys) :-
+    append1(Xs, _, Ys).
 
 
-suffix(Xs, Xs).
-suffix(Xs, [_|Ys]) :-
-    suffix(Xs, Ys).
+%suffix(Xs, Xs).
+%suffix(Xs, [_|Ys]) :-
+%    suffix(Xs, Ys).
+
+suffix(Xs, Ys) :-
+    append1(_, Xs, Ys).
 
 
 % sublist as a suffix of a prefix:
@@ -17,16 +27,16 @@ suffix(Xs, [_|Ys]) :-
 
 
 % sublist as a prefix of a suffix:
-sublist(Xs, Ys) :-
-    prefix(Xs, Ss),
-    suffix(Ss, Ys).
+%sublist(Xs, Ys) :-
+%    prefix(Xs, Ss),
+%    suffix(Ss, Ys).
 
 % recursive definition of sublist:
 % preferred solution:
-%sublist(Xs, Ys) :-
-%    prefix(Xs, Ys).
-%sublist(Xs, [_|Ys]) :-
-%    sublist(Xs, Ys).
+sublist(Xs, Ys) :-
+    prefix(Xs, Ys).
+sublist(Xs, [_|Ys]) :-
+    sublist(Xs, Ys).
 
 
 % HW3.2 answers:
@@ -55,9 +65,6 @@ member(X, [_|Ys]) :-
 %     plus(X,Y,Z).
 
 
-concatenate_lists([], Ys, Ys).
-concatenate_lists([X|Xs], Ys, [X|Zs]) :-
-    concatenate_lists(Xs, Ys, Zs).
 
 % ?- concatenate_lists(X,Y, [1,2]).
 % X = [],
