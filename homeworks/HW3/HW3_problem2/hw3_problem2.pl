a72c63ac76b9a6a2685985eab1f3b0bfe63fb2f6
ilya.kopyl@salesforce.com <ilya.kopyl@gmail.com>
2018-03-14 21:19:26 -0700

    Started the HW3 problem2.

---
+++ homeworks/HW3/HW3_problem2/hw3_problem2.pl
@@ -0,0 +1,27 @@
+% Operations on lists:
+
+append1([], L2, L3).
+append1([H|T1], L2, [H|T3]) :-
+    append1(T1, T2, L3).
+
+prefix(Prefix, List) :-
+    append1(Prefix, _, List).
+
+suffix(Suffix, List) :-
+    append1(_, Suffix, List).
+
+% sublist is a prefix of a suffix of a list
+sublist(Sublist, List) :-
+    prefix(Sublist, Suffix),
+    suffix(Suffix, List).
+
+member_of_list(X, List) :-
+    sublist([X], List).
+
+first_element_of_list(X1, List) :-
+    prefix([X1], List).
+
+last_element_of_list(Xn, List) :-
+    suffix([Xn], List).
+
+
