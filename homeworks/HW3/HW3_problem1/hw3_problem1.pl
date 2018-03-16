6b94075cf45a863e296c664da4e76eb5c7d14ca1
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-15 18:40:24 -0700

    Doing the steady progress on HW3 problem1

--- homeworks/HW3/HW3_problem1/hw3_problem1.pl
+++ homeworks/HW3/HW3_problem1/hw3_problem1.pl
@@ -1,3 +1,52 @@
 % Family relationships
 
 
+m([
+    'Prince Andrew of Greece and Denmark',
+    'King George VI',
+    'Prince Philip',
+    'Antony Armstrong-Jones',
+    'Prince Charles',
+    'Mark Philips',
+    'Prince Andrew',
+    'David Armstrong-Jones',
+    'Prince Edward',
+    'Peter Philips',
+    'Jasper Cable-Alexander',
+    'Prince William',
+    'Prince Harry',
+    'James, Viscount Severn'
+]).
+
+f([
+    'Princess Alice of Battenberg',
+    'Elizabeth the Queen Mother',
+    'Queen Elizabeth II',
+    'Princess Margaret',
+    'Someone else',
+    'Princess Diana',
+    'Princess Anne',
+    'Polly Higson',
+    'Sarah Margaret Ferguson',
+    'Sophie Helen Rhys-Jones',
+    'Lady Sarah Chatto',
+    'Zara Tindall',
+    'Lady Frances von Hofmannsthal',
+    'Princess Beatrice',
+    'Princess Eugenie',
+    'Lady Louise Windsor'
+]).
+
+family([
+    'Prince Andrew of Greece and Denmark',
+    'Princess Alice of Battenberg',
+    ['Prince Philip']
+]).
+
+
+
+family([]).
+family([]).
+family([]).
+
+
