7b761ec59659cae84e1a198a923997c1ef44e933
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-21 17:50:35 -0700

    Debugging rsum.

--- midterm_practice/mt00/mt00.c
+++ midterm_practice/mt00/mt00.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+int rsum(int);
+int isum(int);
+
 int main()
 {
 
@@ -8,6 +11,8 @@ int main()
     unsigned char num02 = 0b00000010u;
     unsigned char num03 = 0b00000011u;
     unsigned char num04 = 0b00000100u;
+
+    unsigned char numWhatever = 0b00111000u;
 
     unsigned char num65 = 0b01000001u;  // 65 == A == 2^6 + 2^0
 
@@ -18,6 +23,37 @@ int main()
     printf("num04 = %u\n", num04);
 
     printf("num65 = %c\n", num65);
+    printf("isum(num65) == %d\n", isum(num65));
+    printf("rsum(num65) == %d\n", rsum(num65));
+
+
+    printf("numWhatever = %u\n", numWhatever);
+    printf("isum(numWhatever) == %d\n", isum(numWhatever));
+    printf("rsum(numWhatever) == %d\n", rsum(numWhatever));
 
     return 0;
+}
+
+int rsum(int num)
+{
+    int numberOfBits = 0;
+
+    if (num & 1 == num)
+        return num;
+
+    numberOfBits += (num >> 1) & 1;
+    numberOfBits += rsum(num >> 1);
+
+    return numberOfBits;
+}
+
+int isum(int num)
+{
+    int sumOfBits = 0;
+    int numberOfBits = sizeof(num) * 8;
+
+    for (int i = 0; i < numberOfBits; i++)
+        sumOfBits += ((num >> i) & 1);
+
+    return sumOfBits;
 }
