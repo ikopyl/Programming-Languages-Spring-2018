1a8cd843f5c74f4550329563d800f5a06d220f44
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-21 18:03:17 -0700

    Geeks for geeks helped me.

--- midterm_practice/mt00/mt00.c
+++ midterm_practice/mt00/mt00.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int rsum(int);
-int isum(int);
+int isum1(int);
+int isum2(int);
 
 int main()
 {
@@ -23,12 +24,14 @@ int main()
     printf("num04 = %u\n", num04);
 
     printf("num65 = %c\n", num65);
-    printf("isum(num65) == %d\n", isum(num65));
+    printf("isum1(num65) == %d\n", isum1(num65));
+    printf("isum2(num65) == %d\n", isum2(num65));
     printf("rsum(num65) == %d\n", rsum(num65));
 
 
     printf("numWhatever = %u\n", numWhatever);
-    printf("isum(numWhatever) == %d\n", isum(numWhatever));
+    printf("isum1(numWhatever) == %d\n", isum1(numWhatever));
+    printf("isum2(numWhatever) == %d\n", isum2(numWhatever));
     printf("rsum(numWhatever) == %d\n", rsum(numWhatever));
 
     return 0;
@@ -36,18 +39,13 @@ int main()
 
 int rsum(int num)
 {
-    int numberOfBits = 0;
-
-    if (num & 1 == num)
-        return num;
-
-    numberOfBits += (num >> 1) & 1;
-    numberOfBits += rsum(num >> 1);
-
-    return numberOfBits;
+    if (num == 0)
+        return 0;
+    else
+        return 1 + rsum(num & (num - 1));       // Brian Kernigan's Algorithm
 }
 
-int isum(int num)
+int isum1(int num)
 {
     int sumOfBits = 0;
     int numberOfBits = sizeof(num) * 8;
@@ -55,5 +53,16 @@ int isum(int num)
     for (int i = 0; i < numberOfBits; i++)
         sumOfBits += ((num >> i) & 1);
 
+    return sumOfBits;
+}
+
+int isum2(int num)
+{
+    int sumOfBits = 0;
+    while (num)
+    {
+        sumOfBits += num & 1;
+        num = num >> 1;
+    }
     return sumOfBits;
 }
