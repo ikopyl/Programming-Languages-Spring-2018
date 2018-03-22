012ca1678003c783b2362e887c26211fbadf2c77
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-22 11:20:45 -0700

    mt00 is ready and is fully tested.

--- midterm_practice/mt00/mt00.c
+++ midterm_practice/mt00/mt00.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int rsum(int);
+int rsum1(int);
+int rsum2(int);
 int isum1(int);
 int isum2(int);
 int isum3(int);
@@ -28,24 +29,45 @@ int main()
     printf("isum1(num65) == %d\n", isum1(num65));
     printf("isum2(num65) == %d\n", isum2(num65));
     printf("isum3(num65) == %d\n", isum3(num65));
-    printf("rsum(num65) == %d\n", rsum(num65));
+    printf("rsum1(num65) == %d\n", rsum1(num65));
+    printf("rsum2(num65) == %d\n", rsum2(num65));
 
 
     printf("numWhatever = %u\n", numWhatever);
     printf("isum1(numWhatever) == %d\n", isum1(numWhatever));
     printf("isum2(numWhatever) == %d\n", isum2(numWhatever));
     printf("isum3(numWhatever) == %d\n", isum3(numWhatever));
-    printf("rsum(numWhatever) == %d\n", rsum(numWhatever));
+    printf("rsum1(numWhatever) == %d\n", rsum1(numWhatever));
+    printf("rsum2(numWhatever) == %d\n", rsum2(numWhatever));
+
+
+    int n1 = 6;
+    int n2 = -1;
+
+    printf("n1 = %d\n", n1);
+    printf("isum3(n1) == %d\n", isum3(n1));
+    printf("rsum2(n1) == %d\n", rsum2(n1));
+    printf("n2 = %d\n", n2);
+    printf("isum3(n2) == %d\n", isum3(n2));
+    printf("rsum2(n2) == %d\n", rsum2(n2));
 
     return 0;
 }
 
-int rsum(int num)
+int rsum1(int num)
 {
     if (num == 0)
         return 0;
     else
-        return 1 + rsum((num - 1) & num);       // Brian Kernigan's Algorithm
+        return 1 + rsum1((num - 1) & num);       // Brian Kernigan's Algorithm
+}
+
+int rsum2(int num)
+{
+    if (num == 0)
+        return 0;
+    else
+        return (num & 1) + rsum2(num >> 1);
 }
 
 int isum1(int num)
