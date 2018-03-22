9abce758ea756ebb0d6cde98f24b79511a41302d
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-21 17:04:01 -0700

    Minor formatting changes.

--- midterm_practice/mt00/mt00.c
+++ midterm_practice/mt00/mt00.c
@@ -3,19 +3,19 @@
 int main()
 {
 
-    unsigned char num0 = 0b00000000u;
-    unsigned char num1 = 0b00000001u;
-    unsigned char num2 = 0b00000010u;
-    unsigned char num3 = 0b00000011u;
-    unsigned char num4 = 0b00000100u;
+    unsigned char num00 = 0b00000000u;
+    unsigned char num01 = 0b00000001u;
+    unsigned char num02 = 0b00000010u;
+    unsigned char num03 = 0b00000011u;
+    unsigned char num04 = 0b00000100u;
 
     unsigned char num65 = 0b01000001u;  // 65 == A == 2^6 + 2^0
 
-    printf("num0 = %u\n", num0);
-    printf("num1 = %u\n", num1);
-    printf("num2 = %u\n", num2);
-    printf("num3 = %u\n", num3);
-    printf("num4 = %u\n", num4);
+    printf("num00 = %u\n", num00);
+    printf("num01 = %u\n", num01);
+    printf("num02 = %u\n", num02);
+    printf("num03 = %u\n", num03);
+    printf("num04 = %u\n", num04);
 
     printf("num65 = %c\n", num65);
 
