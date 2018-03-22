8811c788bb6af1c45a420b1667d4efa39953cd1e
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-22 16:45:15 -0700

    Starting my work on prolog_mt00

---
+++ midterm_practice/mt00/prolog_mt00.pl
@@ -0,0 +1,17 @@
+% practicing Prolog MT00
+
+population('China', 1347).
+population('India', 1210).
+population('USA', 314).
+population('Indonesia', 237).
+population('Brasil', 193).
+population('Japan', 127).
+
+area('China', 9.5).
+area('India', 2.9).
+area('USA', 9.1).
+area('Indonesia', 1.8).
+area('Brasil', 8.4).
+area('Japan', 0.36).
+
+
