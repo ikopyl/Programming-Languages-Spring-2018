c657ec3bace773e8ad9258d5ad41c267acfe14e7
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-27 23:06:40 -0700

    The mt02.c is ready

--- midterm_practice/mt02/mt02.c
+++ midterm_practice/mt02/mt02.c
@@ -17,7 +17,15 @@ int main()
     printf("%d\n", result);
     printCharBitPattern(result);
 
+    puts("");
 
+    char n1 = 3;
+    printf("%d\n", n0);
+    printCharBitPattern(n0);
+
+    char result1 = copyReverse(n1, 7);
+    printf("%d\n", result1);
+    printCharBitPattern(result1);
 
     return 0;
 }
