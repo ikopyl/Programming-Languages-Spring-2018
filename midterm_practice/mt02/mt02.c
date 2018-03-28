854eadbdc2d60955a486a0f97debc376eb7275bc
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-27 23:03:33 -0700

    The prototype of mt02 is ready

--- midterm_practice/mt02/mt02.c
+++ midterm_practice/mt02/mt02.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
+#include <math.h>
 #include "functions.h"
+
+char copyReverse(char, unsigned char);
 
 int main()
 {
 
-    char n0 = 0;
-    char n1 = 1;
-    char n2 = 2;
-    char n3 = 3;
-    unsigned char n4 = 128;
+    puts("");
 
+    char n0 = 3;
+    printf("%d\n", n0);
     printCharBitPattern(n0);
-    printCharBitPattern(n1);
-    printCharBitPattern(n2);
-    printCharBitPattern(n3);
-    printCharBitPattern(n4);
+
+    char result = copyReverse(n0, 3);
+    printf("%d\n", result);
+    printCharBitPattern(result);
+
+
 
     return 0;
 }
 
+char copyReverse(char m, unsigned char n)
+{
+    char k = 0;
+
+    while (n--) {
+        k += (m & 1);
+        k = k << 1;
+        m = m >> 1;
+    }
+    return k;
+}
