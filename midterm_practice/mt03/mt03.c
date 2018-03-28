00a83e4181adf90528408359cc1e88213c59d7b9
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-03-27 19:16:23 -0700

    Current progress with array of chars

--- midterm_practice/mt03/mt03.c
+++ midterm_practice/mt03/mt03.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
 #include "functions.h"
 
-void align(int *, int);
+void align(char *, int);
 
 int main()
 {
 
-    int s[] = { 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 0, 0, 0 };
-    printIntArray(s, sizeof(s));
-    align(s, 13);
+    puts("");
+
+    char s[] = { ' ', ' ', ' ', ' ', ' ', ' ', 'f', 'i', 'r', 's', 't', ' ', ' ', ' ', ' ', 'l', 'a', 's', 't', '\0', ' ', ' ', ' ', ' ' };
+    printCharArray(s, sizeof(s));
+    align(s, 24);
+
+    puts("");
+
+    char s1[] = { ' ', ' ', 'f', 'i', 'r', 's', 't', ' ', ' ', ' ' };
+    printCharArray(s1, sizeof(s1));
+    align(s1, 10);
+
+    puts("");
+
+    char s2[] = { ' ', ' ', 'f', 'i', 'r', 's', 't', '\0', ' ', ' ' };
+    printCharArray(s2, sizeof(s2));
+    align(s2, 10);
+
+    puts("");
+
+    char s3[] = { ' ', ' ', 'f', 'i', 'r', 's', 't', ' ', ' ', '\0' };
+    printCharArray(s3, sizeof(s3));
+    align(s3, 10);
+
+    puts("");
 
     return 0;
 }
 
-void align(int * s, int n) 
+void align(char * s, int n) 
 {
     int count = 0;
     while (s[count] == 0)
         count++;
-    printf("Index of first non-zero element in the array is %d\n", count);
+    printf("First non-zero element index: %d\n", count);
+
+    for (int i = 0; i < n; i++, count++) 
+    {
+        s[i] = s[count];
+        if (s[count] == EOF || count >= n) 
+        {
+            printf("Index of EOF: %d\n", count);
+            count++;
+            break;
+        }
+    }
 
 }
