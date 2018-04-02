5c1c9170b643bcb4406d687a5065383a718cabd8
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-04-01 19:09:00 -0700

    Minor formatting changes

--- scheme_practice/sum_n_fib.rkt
+++ scheme_practice/sum_n_fib.rkt
@@ -2,9 +2,10 @@
 
 (define (sum ls)
   (if (null? ls) 0
-      (+ (car ls) (sum (cdr ls)))))
+      (+ (car ls)
+         (sum (cdr ls)))))
 
 (define (fib n)
   (if (<= n 1) n
-      (+ (fib (- n 1)) (fib (- n 2)))))
-
+      (+ (fib (- n 1))
+         (fib (- n 2)))))
