b9e2e037980d944fdfc25ec70fe6fca52c7e70e1
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-04-01 23:18:32 -0700

    Adding flatten1

--- scheme_practice/sum_n_fib.rkt
+++ scheme_practice/sum_n_fib.rkt
@@ -9,3 +9,18 @@
   (if (<= n 1) n
       (+ (fib (- n 1))
          (fib (- n 2)))))
+
+;; checkout racket/collects/racket/list.rkt
+(define (flatten1 sequence)
+    (cond 
+           [(empty? sequence) null]
+           
+           [(list? (car sequence))
+            (append (flatten1 (car sequence))
+                    (flatten1 (cdr sequence)))]
+           
+           [else (cons (car sequence)
+                       (flatten1 (cdr sequence)))]
+           
+           )
+  )
