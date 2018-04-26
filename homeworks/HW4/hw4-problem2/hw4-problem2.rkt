3dd5f1f3a342ec555fe4e9bce3bc00cefb7c34da
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-04-25 20:15:15 -0700

    HW4 problem2 is complete

--- homeworks/HW4/hw4-problem2/hw4-problem2.rkt
+++ homeworks/HW4/hw4-problem2/hw4-problem2.rkt
@@ -1,6 +1,5 @@
 #lang racket
 
-
 (define (square x)
   (* x x))
 
@@ -9,9 +8,6 @@
 
 ; at least 1 argument is required
 (define (sqr x . y)
-  (cons (square x) (squarelist y)))
-
-(define (sqr-new x . y)
   (squarelist (cons x y)))
 
 
@@ -22,27 +18,28 @@
 (define (sum . x)
   (sumlist x))
 
+
 ; at least 1 argument is required
-(define (mean_old x . y)
+(define (mean x . y)
   (/ (sum x (sumlist y))
      (+ 1 (length y))))
 
-(define (mean lst)
+(define (meanlist lst)
   (/ (sumlist lst)
      (length lst)))
 
 
-
-; initial version of the function that takes a list of arguments:
-(define (sigma-old lst)
-  (sqrt (-
-         (/ (apply + (map square lst)) (length lst))
-         (square (/ (apply + lst) (length lst))))))
-
 ; second attempt:
+(define (sigma-old x . y)
+  (sqrt (- (meanlist (squarelist (cons x y)))
+           (square (meanlist (cons x y))))))
+
+; let's introduce a local scope:
 (define (sigma x . y)
-  (sqrt (- (mean (sqr (sum x y)))
-           (sqr (mean x y)))))
+  (let ((mydata (cons x y)))
+    (sqrt (- (meanlist (squarelist mydata))
+             (square (meanlist mydata))))))
+
 
 
 
