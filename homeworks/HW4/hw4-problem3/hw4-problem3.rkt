5bfb32770662648594640b274ef9aea90f2e5959
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-04-26 12:49:51 -0700

    HW4 problem 3a is complete

---
+++ homeworks/HW4/hw4-problem3/hw4-problem3.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (non-negative? x)
+  (if (or (equal? x 0)
+          (positive? x)) #t #f))
+
+(define (valid-input? x)
+  (if (and (number? x)
+           (and (integer? x)
+                (non-negative? x))) #t #f))
+
+(define (line x)
+  (if (valid-input? x)
+      (if (equal? x 0) (newline)
+          (begin (display '*)
+                 (line (- x 1))))
+      (display "Argument must be a non-negative integer.")))
