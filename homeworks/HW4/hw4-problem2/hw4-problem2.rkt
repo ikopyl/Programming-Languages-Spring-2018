1ec7bfe1af16ba6201877cafa2c4e961becf4dba
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-04-25 18:57:23 -0700

    Making a steady progress on problem2

--- homeworks/HW4/hw4-problem2/hw4-problem2.rkt
+++ homeworks/HW4/hw4-problem2/hw4-problem2.rkt
@@ -4,6 +4,20 @@
 (define (square x)
   (* x x))
 
+(define (squarelist x)
+  (map square x))
+
+; function definition requires at least 1 argument
+(define (sqr x . y)
+  (cons (square x) (squarelist y)))
+
+(define (sumlist x)
+  (apply + x))
+
+; there are no required arguments, because sum of nothing is zero
+(define (sum . x)
+  (sumlist x))
+
 ; initial version of the function that takes a list of arguments:
 (define (sigma lst)
   (sqrt (-
