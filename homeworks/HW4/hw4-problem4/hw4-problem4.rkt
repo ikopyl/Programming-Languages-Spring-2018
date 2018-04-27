f41d8a5f26e3dbe9be1c71b8fc202cd1fd7221a1
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-04-26 19:48:04 -0700

    Fixed a bug

--- homeworks/HW4/hw4-problem4/hw4-problem4.rkt
+++ homeworks/HW4/hw4-problem4/hw4-problem4.rkt
@@ -11,29 +11,38 @@
 
 ; auxiliary predicate
 (define (difference-sufficiently-small? x1 x2)
-  (let ((threshold 1e-6))
+  (let ((threshold 1e-10))
     (if (< (abs (- x2 x1)) threshold) #t #f)))
 
-(define (disp x n)
-  (display (/ (round (* x (expt 10 n)))
-              (expt 10 n))))
+(define (round-to-n-significant-decimal-digits x n)
+  (/ (round (* x (expt 10 n)))
+     (expt 10 n)))
 
-; auxiliary function
+; auxiliary function - refactoring code for improved readability
 (define (mean x . y)
   (/ (apply + (cons x y))
      (+ 1 (length y))))
 
+; auxiliary function - refactoring code for improved readability
+(define (third-of-delta-between x1 x2)
+  (/ (- x2 x1) 3))
+
 (define (fmax f x1 x2)
-  (cond
-    [(difference-sufficiently-small? x1 x2) (display "maximum: f(")
-                                            (disp (mean x1 x2) 4)
-                                            (display ") = ")
-                                            (disp (f (mean x1 x2)) 4)]
-    [else (let ((a1 (+ x1 (/ (- x2 x1) 3)))
-                (a2 (- x2 (/ (- x2 x1) 3))))
-            (if (< (f a1) (f a2))
-                (fmax f a1 x2)
-                (fmax f x1 a2)))]))
+  (if (not (procedure? f)) (display "First argument must be a procedure.")
+      (cond
+        [(difference-sufficiently-small? x1 x2)
+         (let ((rounded-xmax (round-to-n-significant-decimal-digits (mean x1 x2) 6)))
+           (display "xmax = ")
+           (display rounded-xmax)
+           (newline)
+           (display "ymax = ")
+           (display (round-to-n-significant-decimal-digits (f rounded-xmax) 6)))]
+        [else (let ((a1 (+ x1 (third-of-delta-between x1 x2)))
+                    (a2 (- x2 (third-of-delta-between x1 x2))))
+                (if (< (f a1) (f a2))
+                    (fmax f a1 x2)
+                    (fmax f x1 a2)))])))
+
 
 
 ; (fmax (lambda(x) (* x (- 1 x))) 0 10)
