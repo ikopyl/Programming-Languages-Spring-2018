2dc1d7a3f3d9888e2c1bef9e13db833c63a7bf22
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-05-03 14:23:55 -0700

    Changes in HW4 problem1

--- homeworks/HW4/hw4-problem1/hw4-problem1.rkt
+++ homeworks/HW4/hw4-problem1/hw4-problem1.rkt
@@ -1,4 +1,4 @@
-#lang scheme
+#lang racket
 
 ; problem 1a: show an example of the anonymous function and its use
 
@@ -15,13 +15,57 @@
 ; problem 1d: show an example of comparing functions and lists for equality
 
 
+
+
+
+
+
 ; problem 1e: show an example of passing function as an argument to another function
+
+(define (square x)
+    (* x x))
+
+(define (double x)
+    (* x 2))
+
+(define (atom? x)
+  (and (not (pair? x))
+       (not (null? x))))
+
+(define (conveyor lst-functions lst-data-operand1)
+    (cond
+      [(empty? lst-functions) lst-data-operand1]
+      [(atom? lst-functions) ((map lst-functions lst-data-operand1))]
+      [else (conveyor (cdr lst-functions)
+                      (if (procedure? (car lst-functions))
+                          (map (car lst-functions) lst-data-operand1)
+                          (map (eval (car lst-functions)) lst-data-operand1)))]))
 
 
 ; problem 1f: show an example of returning a function as a result of another function
+
+
+; There are only two hard things in Computer Science: cache invalidation and naming things.
+(define (multiple-fun x)
+  (cond
+    [(number? x) (lambda(d) (* d d))]
+    [(procedure? x) (lambda(d) (* d d))]
+    [(atom? x) (lambda(d) (display d))]
+    [else (lambda(d) (car d))]))
+;  (if (number? x)
+;      (lambda(d) (* d d))
+;      (lambda(d) (display d))))
 
 
 ; problem 1g: show examples of:
 ;    reading function(s) from the keyboard,
 ;    reading function(s) from a file,
 ;    and displaying a function
+
+
+(define (read-fun-from-keyboard)
+  (eval (read)))
+
+(define (execute-from-keyboard x)
+  ((read-fun-from-keyboard) x))
+
