0fadec13e2968de362cf8a93f49d392caf764b87
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-05-02 19:42:00 -0700

    Current progress

--- homeworks/HW5/hw5-problem1/hw5_problem1.rb
+++ homeworks/HW5/hw5-problem1/hw5_problem1.rb
@@ -2,4 +2,10 @@ def generate_new_rand_int_arr(size = 100, val_upper_bound = 10)
   Array.new(size) { rand(val_upper_bound + 1) }
 end
 
+def show(array)
+  print 'Array is empty' if array.empty?
+  array.each { |item| print item, ' ' } unless array.empty?
+  puts
+end
+
 p generate_new_rand_int_arr
