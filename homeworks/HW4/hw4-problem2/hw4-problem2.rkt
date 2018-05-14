2d6ec915de8d6e675e20e08d11aa4a72f515d167
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-05-14 16:52:01 -0700

    Added computation of varco

--- homeworks/HW4/hw4-problem2/hw4-problem2.rkt
+++ homeworks/HW4/hw4-problem2/hw4-problem2.rkt
@@ -63,12 +63,19 @@
 ;  (let ((variance (variance-lst lst)))
 ;    (sqrt variance)))
 
+
+(define grades '(100 94 92 88 87 87 86 82 82 80 80 76 75 75 73 73 73 72 72 71 71 70 70 70 69 69 69 68 68 67 67 67 65 64 63 63 62 61 61 59 58 58 57 56 56 56 56 56 54 53 48 46 46 46 45 45 45 45 45 44 44 42 42 41 40 40 40 39 39 38 38 37 37 36 35 34 34 34 33 32 31 30 28 27 26 25 24 24 22 22 22 21 21 20 19 19 18 18 18 18 18 16 16 16 15 13 13 13 12 10 8 4))
+
 (define (variance-lst lst)
   (- (mean-lst (square-lst lst))
      (square (mean-lst lst))))
 
 (define (sigma-lst lst)
   (sqrt (variance-lst lst)))
+
+
+(define (varco-lst lst)
+  (/ (sigma-lst lst) (mean-lst lst)))
 
 
 
